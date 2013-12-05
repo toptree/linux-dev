8960581372c1c2b98a22d728939547f365aae33e
toptree <toptreegzh@163.com>
2013-12-04 22:24:17 -0800

    操作服务器日志

---
+++ shell/log_del_expire.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+#############################################
+# Scripts for delete expire loggs
+# Written by toptreegzh
+# at 2010-08-04
+#############################################
+
+logType=$1
+expireDay=$2
+dirPath=$3
+
+if [ ${dirPath}x == "x"  ] || [ ${expireDay}x == "x" ] || [ ${logType}x == "x" ];
+then
+	echo "输入参数有误 1：日志类型 2：保留天数 3：日志路径";
+	exit -1;
+fi
+
+#运行命令
+#find "${dirPath}" -mtime +${expireDay}  -type f -name "${logType}" | xargs rm -rf
+
+find "${dirPath}" -mtime +${expireDay}  -type f -name "${logType}" 
+
